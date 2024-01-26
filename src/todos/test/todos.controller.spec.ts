06f2a8cee510ea281ef01715e92f1aca92d2ad03
matejkostov <matej4o9@gmail.com>
2024-01-26 20:07:18 +0200

    fix errors

--- src/todos/test/todos.controller.spec.ts
+++ src/todos/test/todos.controller.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { TodosController } from './todos.controller';
+import { TodosController } from '../todos.controller';
 
 describe('TodosController', () => {
   let controller: TodosController;
