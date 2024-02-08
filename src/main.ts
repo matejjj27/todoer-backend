52c134958b5d13cda784d9cb6049774e6391cb0b
matejkostov <matej4o9@gmail.com>
2024-02-08 16:57:49 +0200

    add origin

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
+    origin: 'https://todoer-app-zeta.vercel.app',
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   });
   await app.listen(3000);
