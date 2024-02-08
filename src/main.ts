1941c0b1efc22ca3b1a6b35ab4a81a4acaf410ad
matejkostov <matej4o9@gmail.com>
2024-02-08 16:59:10 +0200

    cors is enabled for all routes

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'https://todoer-app-zeta.vercel.app',
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-  });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
