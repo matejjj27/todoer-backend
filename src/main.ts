89cd3d2aa62e72ecd3b4bbecfe2102f7953d0409
matejkostov <matej4o9@gmail.com>
2024-02-08 16:41:12 +0200

    allow request from all

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'https://todoer-app-zeta.vercel.app', // Allow requests from this origin
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'], // Allow only GET and POST requests
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
-  });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
