7590953b664dc08b0a0e4e4deefd81e531b8b97b
matejkostov <matej4o9@gmail.com>
2024-02-08 16:34:48 +0200

    enable cors only for frontend

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://todoer-app-zeta.vercel.app', // Allow requests from this origin
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'], // Allow only GET and POST requests
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
+  });
   await app.listen(3000);
 }
 bootstrap();
