0b2f76a4e452888661b9d671c3acf6aaaa8493ff
matejkostov <matej4o9@gmail.com>
2024-02-08 16:50:05 +0200

    include methods in cors setup

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://todoer-app-zeta.vercel.app',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  });
   await app.listen(3000);
 }
 bootstrap();
