d878b092ab8cc5c599b4085d1fcfeb57ddeecf30
matejkostov <matej4o9@gmail.com>
2024-02-08 17:26:21 +0200

    add localhost for testing purposes

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://todoer-app-zeta.vercel.app'],
+    origin: ['https://todoer-app-zeta.vercel.app', 'http://localhost:5173'],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
